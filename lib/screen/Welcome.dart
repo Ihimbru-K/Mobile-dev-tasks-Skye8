3d90d627c6bd009cc7e2b8d57a292f776c6defcc
Ihimbru-K <kanihims25@gmail.com>
2023-03-26 11:55:41 +0100

    working on welcome screen task

---
+++ lib/screen/Welcome.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  class welcome() extends StatefulWidget {
+    const w({Key? key}) : super(key: key);
+
+    @override
+    State<w> createState() => _wState();
+  }
+
+  class _wState extends State<w> {
+    @override
+    Widget build(BuildContext context) {
+      return const Placeholder();
+    }
+  }
+
+
+
+
+
+}
